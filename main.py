5172142aacc5b12b38c9a568013f293dfbf821ba
Nostalogicwh <apocalypsewh@163.com>
2020-11-16 15:38:37 +0800

    update

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import numpy as np
 import open3d as o3d
 
 print("Load a ply point cloud, print it, and render it")
-pcd1 = o3d.io.read_point_cloud("PointCould.xyz")  # 这里的cat.ply替换成需要查看的点云文件
-o3d.visualization.draw_geometries([pcd1])
 
+# pcd = o3d.io.read_point_cloud("cup.xyz")
+# o3d.visualization.draw_geometries([pcd])
+
+# pcd1 = o3d.io.read_point_cloud("PointCould.xyz")  # 这里的cat.ply替换成需要查看的点云文件
+# o3d.visualization.draw_geometries([pcd1])
+#
 pcd2 = o3d.io.read_point_cloud("fandisk1.xyz")
 o3d.visualization.draw_geometries([pcd2])
 
